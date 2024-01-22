8d757ebd3348cdc7ecbf64615a1cff65669668b0
harshgolchha2003 <harshgolchha48@gmail.com>
2024-01-22 19:08:08 +0530

    modifications

--- src/App.js
+++ src/App.js
@@ -19,7 +19,11 @@ function App() {
   const [account, setAccount] =useState(null)
   const loadBlockachainData=async()=>{
     const provider=new ethers.providers.Web3Provider(window.ethereum)
-   
+    window.ethereum.on('accountsChanged',async()=>{
+      const accounts=await window.ethereum.request({method:'eth_requestAccounts'});
+      const account=ethers.utils.getAddress(accounts[0]);
+      setAccount(account)
+    })
   }
   useEffect(()=>{
     loadBlockachainData();
