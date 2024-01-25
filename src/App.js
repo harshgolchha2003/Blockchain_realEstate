5a7476fdc715e4077f92482717c2295e86122617
harshgolchha2003 <harshgolchha48@gmail.com>
2024-01-25 10:35:29 +0530

    FrontEnd

--- src/App.js
+++ src/App.js
@@ -16,12 +16,36 @@ import Escrow from './abis/Escrow.json'
 import config from './config.json';
 
 function App() {
+  const[homes,setHomes]=useState(null)
   const [account, setAccount] =useState(null)
+  const[provider,setProvider]=useState(null)
+  const[escrow,setEscrow]=useState(null)
   const loadBlockachainData=async()=>{
     const provider=new ethers.providers.Web3Provider(window.ethereum)
+
+    setProvider(provider)
+
+    const network=await provider.getNetwork()
+
+    // config[network.chainId].escrow.address
+    const realEstate=new ethers.Contract( config[network.chainId].realEstate.address,RealEstate,provider)
+    const totalSupply=await realEstate.total_Supply()
+    var homes=[];
+    for(let i=1;i<=totalSupply;i++){
+      const uri=await realEstate.tokenURI(i);
+      const response=await fetch(uri)
+      const metadata=await response.json()
+      homes.push(metadata);
+    }
+    setHomes(homes)
+    // console.log(homes)
+    const escrow=new ethers.Contract( config[network.chainId].escrow.address,Escrow,provider)
+    setEscrow(escrow);
+
     window.ethereum.on('accountsChanged',async()=>{
       const accounts=await window.ethereum.request({method:'eth_requestAccounts'});
       const account=ethers.utils.getAddress(accounts[0]);
+
       setAccount(account)
     })
   }
@@ -31,10 +55,34 @@ function App() {
   return (
     <div>
       <Navigation account={account} setAccount={setAccount}/>
+      <Search/>
       <div className='cards__section'>
 
-        <h3>Welcome to Millow!</h3>
+        <h3>Homes for You</h3>
+          <hr/>
+        <div className='cards'>
+         <script>
+          console.log()
+          console.log(homes)
+         </script>
+            {homes.map((home,index)=>( 
+             <div className='card' key="index">
+                <div className='card__image'>
+                  <img src="home.image" alt="Home"/>
+                </div>
 
+              <div className='card__info'>
+                <h4>${home.attributes[0].value}ETH</h4>
+                <p>
+                  <strong>${home.attributes[2].value}</strong> beds|
+                  <strong>${home.attributes[3].value}</strong> bathrooms|
+                  <strong>${home.attributes[4].value}</strong>  sqft
+                </p>
+                <p>${home.address}</p>
+              </div>
+            </div>
+              ))}  
+        </div>
       </div>
 
     </div>
